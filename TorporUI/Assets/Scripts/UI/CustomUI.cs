a2cd2189b6a7fd970f8e6e7d2b7c5a739abea5ea
Nick Preda <nicholas.preda@gmail.com>
2021-05-12 00:41:50 +0200

    Foundational stuff

---
+++ TorporUI/Assets/Scripts/UI/CustomUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+namespace Custom.UI
+{
+    [ExecuteInEditMode()]
+    public class CustomUI : MonoBehaviour
+    {
+        public ButtonSkinData skinData;
+        public bool _isDirty;
+
+        protected virtual void OnSkinUI()
+        {
+            _isDirty = true;
+        }
+
+        public virtual void Awake()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+            if(!_isDirty) return;
+            OnSkinUI();
+        }
+
+        private void OnValidate ()
+        {
+            _isDirty = true;
+        }
+    }
+}
+
---
+++ TorporUI/Assets/Scripts/UI/UIButton.cs
@@ -0,0 +1,85 @@
+﻿using Custom.UI;
+using Tools.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(IMouseInput))]
+[RequireComponent(typeof(Image))]
+public class UIButton : CustomUI
+{
+
+    //----------------------------------------------------------------------
+    #region UnityElements
+
+    [SerializeField] private Image icon;
+    [SerializeField] private TMP_Text content;
+    private Image background;
+    private IMouseInput mInput;
+
+    #endregion
+    //----------------------------------------------------------------------
+    #region SkinVariables
+
+    private bool _isSelected;
+    private Sprite _icon;
+    private Sprite _background;
+    private TMP_FontAsset _font; 
+    private Color _color;  
+    #endregion
+    //----------------------------------------------------------------------
+    public override void Awake()
+    {
+        background = this.gameObject.GetComponent<Image>();
+        mInput = this.gameObject.GetComponent<UiMouseInputProvider>();
+        Subscribe();
+        base.Awake();
+    }
+
+    protected override void OnSkinUI ()
+    {
+        
+        base.OnSkinUI();
+        if(_isSelected)
+        {
+            _background = skinData.selectBackground;
+            _font = skinData.selectFont;
+            _icon = skinData.selectIcon;
+            _color = skinData.selectColor;
+        }else{
+            _background = skinData.unselectBackground;
+            _font = skinData.unselectFont;
+            _icon = skinData.unselectIcon;
+            _color = skinData.unselectColor;
+        }
+
+
+        background.sprite = _background;
+        if(content != null)
+        {
+            content.font = _font;
+            content.color = _color;
+        } 
+
+        if(icon != null) icon.sprite = _icon;
+
+    }
+
+    void OnClick(PointerEventData eventData)
+    {
+        _isSelected = !_isSelected;
+        _isDirty = true;
+    }
+
+    void Subscribe()
+    {
+        mInput.OnPointerUp += OnClick;
+    }
+    
+
+    void Unsubscribe()
+    {
+        mInput.OnPointerUp += OnClick;
+    }
+}
